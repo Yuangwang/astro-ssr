b19e503c9dea16dda6cc3eff40e99ca121d62a3b
yuangwang <yuangwang2@gmail.com>
2024-12-23 14:38:45 -0500

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import node from 'astro-test-wei';
+import node1 from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
+import netlify from '@astrojs/netlify';
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
+});
